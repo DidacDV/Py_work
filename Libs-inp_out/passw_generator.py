a5eafb1fbf408c96a090039db470a18bced9b9bb
DidacDV <113896749+DidacDV@users.noreply.github.com>
2023-07-11 21:39:04 +0200

    Update passw_generator.py

--- Libs-inp_out/passw_generator.py
+++ Libs-inp_out/passw_generator.py
@@ -2,7 +2,7 @@ import random
 import string
 
 def generate_password(lp, ch):
-    return ''.join(random.choice(ch) for i in range(lp))
+    return ''.join(random.choice(ch) for i in range(lp))                                #Gets random chars from the string we created forming a "password"
 
 
 def main():
@@ -10,12 +10,14 @@ def main():
         try :
             lenght_pass = int(input("Insert the desired lenght of the password: "))
             break
-        except ValueError:
+        except ValueError:                                                            #Checks if input is integer
             print("Insert integer")
+            
     choices = string.ascii_letters + string.digits                                       
     question = input("Do you want special characters? ")
     if question.lower() == "yes":
         choices += "~`! @#$%^&*()_-+={[}]|\:;<,>.?/"
+        
     passw = generate_password(lenght_pass, choices)
     print(f"Password generated: {passw}")
 
